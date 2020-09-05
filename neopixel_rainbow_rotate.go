c022bf0901a96020fbcaf626ebf8229e7ff61ca1
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2020-09-05 14:24:29 +0800

    Update neopixel_rainbow_rotate.go

--- neopixel_rainbow_rotate.go
+++ neopixel_rainbow_rotate.go
@@ -13,52 +13,54 @@ import (
 )
 
 const (
-	LED_PIN          = machine.Pin(2)
+	LED_PIN          = machine.D2
 	LED_NUM          = 12
 	LED_MAX_LEVEL    = 64 // max 255
 	LED_ROTATE_DELAY = 50 // ms
 )
 
+var (
+	neoPixel ws2812.Device
+	leds     [LED_NUM]color.RGBA
+)
+
 func main() {
 
 	LED_PIN.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	neoPixel := ws2812.New(LED_PIN)
+	neoPixel = ws2812.New(LED_PIN)
 
-	var leds [LED_NUM]color.RGBA
+	rainbowSet()
+
+	for {
+		rainbowRotate()
+		time.Sleep(time.Millisecond * LED_ROTATE_DELAY)
+	}
+
+}
+
+func rainbowSet() {
 	ledChange := uint8(LED_MAX_LEVEL / (LED_NUM / 3))
 	ledIndex := [3]uint8{0, uint8(LED_NUM / 3), uint8(LED_NUM / 3 * 2)}
-
 	for i := range leds {
-
-		led_color := [3]uint8{0, 0, 0}
+		ledColor := [3]uint8{0, 0, 0}
 		for j := range ledIndex {
 			if abs(int8(uint8(i)-ledIndex[j])) <= ledIndex[1] {
-				led_color[j] = LED_MAX_LEVEL - abs(int8(uint8(i)-ledIndex[j]))*ledChange
+				ledColor[j] = LED_MAX_LEVEL - abs(int8(uint8(i)-ledIndex[j]))*ledChange
 			}
 		}
 		if uint8(i) >= ledIndex[2] {
-			led_color[0] = LED_MAX_LEVEL - (LED_NUM-uint8(i))*ledChange
+			ledColor[0] = LED_MAX_LEVEL - (LED_NUM-uint8(i))*ledChange
 		}
-		leds[i] = color.RGBA{R: led_color[0], G: led_color[1], B: led_color[2]}
-
+		leds[i] = color.RGBA{R: ledColor[0], G: ledColor[1], B: ledColor[2]}
 	}
-
 	neoPixel.WriteColors(leds[:])
-	time.Sleep(time.Millisecond * 10)
+}
 
-	for {
-
-		var ledsTmp []color.RGBA
-
-		ledsTmp = leds[1:LED_NUM]
-		ledsTmp = append(ledsTmp, leds[0])
-		copy(leds[:], ledsTmp[:])
-
-		neoPixel.WriteColors(leds[:])
-		time.Sleep(time.Millisecond * LED_ROTATE_DELAY)
-
-	}
-
+func rainbowRotate() {
+	var ledsTmp []color.RGBA = leds[1:LED_NUM]
+	ledsTmp = append(ledsTmp, leds[0])
+	copy(leds[:], ledsTmp[:])
+	neoPixel.WriteColors(leds[:])
 }
 
 func abs(x int8) uint8 {
